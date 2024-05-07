166e4c623eddb9f061c4f89520832d2132713edc
Michael Eisel <michael.eisel@gmail.com>
2024-05-07 14:31:42 -0400

    set compute cap to 7.0

--- candle-kernels/build.rs
+++ candle-kernels/build.rs
@@ -1,6 +1,9 @@
+use std::env;
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    env::set_var("CUDA_COMPUTE_CAP", "70");
     let builder = bindgen_cuda::Builder::default();
     println!("cargo:info={builder:?}");
     let bindings = builder.build_ptx().unwrap();
